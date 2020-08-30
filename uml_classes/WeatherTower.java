1e2d8bfb45e90690a5fbfa5762f80db6ad5c9266
Marchall Adams <50703766+MAdams1999@users.noreply.github.com>
2020-08-30 03:56:00 +0200

    Create WeatherTower.java
    gets the weather from the weatherprovider

---
+++ uml_classes/WeatherTower.java
@@ -0,0 +1,10 @@
+package uml_classes;
+
+public class WeatherTower extends Tower{
+  public String getWeather(Coordinates coordinates){
+    return WeatherProvider.getProvider().getCurrentWeather(coordinates);
+  }
+  public void changeWeather(){
+    super.conditionsChanged();
+  }
+}
