777cf6389068aa6ffbe3b4dd344b42a3c2153df9
Marchall Adams <50703766+MAdams1999@users.noreply.github.com>
2020-08-30 03:49:58 +0200

    Create Aircraft.java
    code for id, name, coordinates

---
+++ uml_classes/Aircraft.java
@@ -0,0 +1,26 @@
+package uml_classes;
+
+public class Aircraft{
+  protected Coordinates coordinates;
+  private static long idCounter = 0;
+  protected long id;
+  protected String name;
+  public Aircraft(String name, Coordinates coordinates){
+    this.coordinates = coordinates;
+    this.id = nextId();
+    this.name = name;
+  }
+  public Coordinates getCoordinates(){
+    return this.coordinates;
+  }
+  private long nextId(){
+    idCounter++;
+    return idCounter;
+  }
+  public long getId(){
+    return this.id;
+  }
+  public String getName(){
+    return this.name;
+  }
+}
